8bee7e545b3135688c845dc586adbdecfec8a2d4
natneam <se.natneam.mesele@gmail.com>
2021-01-31 04:51:36 +0300

    sorting by date functionality finished

--- Task Manager/assets/js/app.js
+++ Task Manager/assets/js/app.js
@@ -10,7 +10,11 @@ const taskList = document.querySelector('.collection');          //The ul
 const clearBtn = document.querySelector('.clear-tasks');      //the all task clear button
 
 //the reload button at the top right of navigation
-const reloadIcon = document.querySelector('.fa');   
+const reloadIcon = document.querySelector('.fa'); 
+
+// sort by property for the search
+let sortBy = document.querySelector('#sortby');
+let sortedElement = Array();
 
 
 // Add Event Listener 
@@ -30,13 +34,15 @@ taskList.addEventListener('click', removeTask);
 //reloading the page
 reloadIcon.addEventListener('click', () => {location.reload()});
 
+sortBy.addEventListener('change', ()=>reOrder(sortedElement, taskList, sortBy.value=='ascending'?true:false));
+
  // Add New  Task Function definition 
  function addNewTask(e) {
-     e.preventDefault(); //disable form submission
-     if (taskInput.value == ""){
-         taskInput.style.borderColor = "red";
-         return
-     } 
+    e.preventDefault(); //disable form submission
+    if (taskInput.value == ""){
+        taskInput.style.borderColor = "red";
+        return
+    } 
 
      // Create an li element when the user adds a task 
     const li = document.createElement('li');
@@ -51,8 +57,13 @@ reloadIcon.addEventListener('click', () => {location.reload()});
     link.innerHTML = '<i class="fa fa-remove"></i>';
     // Append link to li
     li.appendChild(link);
-    // Append to ul 
-    taskList.appendChild(li);
+    // Append to ul
+    sortedElement.push([Date.now(), li])
+    if (sortBy.value === 'ascending'){
+        reOrder(sortedElement, taskList, true)
+    }else{
+        reOrder(sortedElement, taskList, false)
+    }
     taskInput.value = '';
 }
 
@@ -89,4 +100,18 @@ function removeTask(e) {
 
     }
 
+}
+
+
+function reOrder(arr, renderOn, ascending=true) {
+    if (ascending === true){
+        arr.sort((a,b)=> a[0]-b[0])
+    }else{
+        arr.sort((a,b)=> b[0]-a[0])
+    }
+    renderOn.innerHTML = ''
+    for (val of sortedElement){
+        console.log(val)
+        renderOn.appendChild(val[1]);
+    }
 }
